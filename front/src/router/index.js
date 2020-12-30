3c5f5cb128a2e1cb117c77d3be8749c64ae31e52
Руслан Мельник <melnk300@gmail.com>
2020-12-30 23:44:38 +0500

    Added curses dashboard page

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Curses_dashboard from  '../views/Courses_dash'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/curses',
+    name: 'Curses dashboard',
+    component: Curses_dashboard
   }
 ]
 
---
+++ front/src/views/Courses_dash.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "Courses_dash"
+}
+</script>
+
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
